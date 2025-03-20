54b200a41764a181c81f9e3c410a4ce4291d91c3
Сковорода Виталий Николаевич <skovoroda@spk.ru>
2025-03-20 09:51:34 +0500

    Добавил память, изменил инструкцию, скорректировал описание полей

--- gpt_db/main.py
+++ gpt_db/main.py
@@ -4,18 +4,42 @@ import sqlparse
 from gigachat import GigaChat
 from gigachat.models import Chat, Messages, MessagesRole
 import asyncio
+import datetime
+import gigachat.context
 from rabbitmq import RabbitMQCommunicator  # Импортируем класс для работы с RabbitMQ
 
 class SQLQueryGenerator:
-    def __init__(self, config_path='gpt_db/data/confs/confs.json', instruction_path='gpt_db/data/confs/instruction_sql.txt'):
+    def __init__(self, config_path='gpt_db/data/confs/confs.json', instruction_path='gpt_db/data/confs/instruction_sql.txt', save_dialog=True, dialog_id="1"):        
         self.api_key = self._load_api_key(config_path)
         self.instruction = self._load_instruction(instruction_path)
+        # print(self.instruction)
         self.model = GigaChat(
             credentials=self.api_key,
             scope="GIGACHAT_API_PERS",
-            model="GigaChat-Max",
+            model="GigaChat-2-Max",
             verify_ssl_certs=False,
         )
+        self.messages = [
+                Messages(
+                    role=MessagesRole.SYSTEM,
+                    content=self.instruction
+                )
+        ]
+        self.save_dialog = save_dialog
+        self.dialog_id = dialog_id
+    
+        if not os.path.exists('gpt_db/data/dialogs_cash'):
+            os.makedirs('gpt_db/data/dialogs_cash')
+    
+        if os.path.exists(f'gpt_db/data/dialogs_cash/{self.dialog_id}.json'):
+            with open(f'gpt_db/data/dialogs_cash/{self.dialog_id}.json', 'r', encoding='utf-8') as f:
+                history = json.load(f)
+            for h in history[-4:]:
+                self.messages += [
+                    Messages(
+                        role=MessagesRole.USER if h['role'] == 'user' else MessagesRole.ASSISTANT,
+                        content=h['content']
+                )]
 
     def _load_api_key(self, config_path):
         with open(config_path, 'r', encoding='utf-8') as f:
@@ -29,21 +53,19 @@ class SQLQueryGenerator:
             db_shema = file.read().strip()
         with open('gpt_db/data/confs/divisions.txt', 'r', encoding='utf-8') as file:
             divisions = file.read().strip()
-            
-        return instruct.replace("[otgruzki_structure]", db_shema).replace("[divisions]", divisions)
+        
+        return instruct.replace("[otgruzki_structure]", db_shema).\
+            replace("[divizions]", divisions).replace("[date]", str(datetime.datetime.now())[:10])
 
     def generate_sql_query(self, user_query):
-        payload = Chat(
-            messages = [
-                Messages(
-                    role=MessagesRole.SYSTEM,
-                    content=self.instruction
-                ),
+        self.messages +=[
                 Messages(
                     role=MessagesRole.USER,
-                    content=f"Запрос пользователя:\n{user_query}\n\nСоставь соответствующий SQL-запрос."
+                    content=f"{user_query}\n\n"
                 )
-            ],
+            ]
+        payload = Chat(
+            messages=self.messages,
             temperature=0.01,
             max_tokens=1000
         )
@@ -52,6 +74,31 @@ class SQLQueryGenerator:
         if response and response.choices:
             generated_sql = response.choices[0].message.content.strip()
             generated_sql = generated_sql.replace("```sql", "").replace("```", "")
+            
+            self.messages +=[
+                Messages(
+                    role=MessagesRole.ASSISTANT,
+                    content=generated_sql
+                    )
+            ]
+            # save messages to json file
+            if self.save_dialog:
+                print("Сохраняем историю")
+                history = []
+                for m in self.messages:
+                    if m.role == MessagesRole.USER:
+                        history += [{"role": "user", "content": m.content}]
+                    elif m.role == MessagesRole.ASSISTANT:
+                        history += [{"role":"assistant", "content": m.content}]
+                    # elif m.role == MessagesRole.SYSTEM:
+                    #     history += [{"system": m.content}]
+                print("History: ", history)
+                with open(f'gpt_db/data/dialogs_cash/{self.dialog_id}.json', 'w', encoding='utf-8') as f:
+                    # Сохранем список сообщений в файл
+                    json.dump(history, f, ensure_ascii=False)
+            
+            
+            
 
             # Проверяем корректность синтаксиса через sqlparse
             if self._is_sql_valid(generated_sql):
@@ -79,15 +126,15 @@ class SQLQueryGenerator:
             return False
 
 # Использование RabbitMQ для отправки сгенерированного запроса и получения ответа
-async def send_sql_request_to_rabbitmq(user_query):
+async def send_sql_request_to_rabbitmq(user_query, save_dialog=True, dialog_id="1"):
     # Создаем экземпляр RabbitMQCommunicator для отправки сообщений в очередь 'sql_requests'
     communicator = RabbitMQCommunicator("amqp://guest:guest@localhost/", "sql_requests")
     
     # Генерируем SQL-запрос
-    sql_generator = SQLQueryGenerator()
+    sql_generator = SQLQueryGenerator(save_dialog=save_dialog, dialog_id=dialog_id)
     try:
         generated_sql = sql_generator.generate_sql_query(user_query)
-        print("Сгенерированный SQL-запрос:\n", generated_sql)
+        print("Ответ модели:", generated_sql)
         
         # Отправляем сгенерированный SQL-запрос через RabbitMQ
         response = await communicator.call({"user_query": generated_sql})
@@ -101,10 +148,28 @@ async def send_sql_request_to_rabbitmq(user_query):
         await communicator.close()
 
 def main():
-    user_query = "Назови топ 3 дивизиона за прошлый месяц"
-    
+    # user_query = "Сколько клиентов отгрузилось на дальнем востоке в прошлом году"
+    user_querys = [
+        
+        "Сколько клиентов отгрузилось на дальнем востоке в прошлом году",
+            "Кто из них грузился чаще?",
+        "Какой менеджер совершил больше всего продаж в прошлом месяце?",
+        "В какой день было больше всего отгрузок?",
+        "Скажи материал с самой большой наценкой",
+        "Скажи АГ1 с самой большой наценкой",
+        "Какой менеджер совершил больше всего продаж в прошлом месяце",
+            "посчитай по тоннажу",
+        "Покажи отгрузки за сегодня"
+    ]
     # Запускаем асинхронную задачу для отправки запроса и получения ответа
-    asyncio.run(send_sql_request_to_rabbitmq(user_query))
+    asyncio.run(send_sql_request_to_rabbitmq(user_querys[1], save_dialog=False, dialog_id="2"))
 
 if __name__ == "__main__":
     main()
+
+# """
+# Код дивизиона указывать в комментариях
+# Более подробный комментарий, писать какое поле используем
+# NULLIF поменять на IFNULL
+# Запрос без срока не писать, по умолчанию - текущий месяц
+# """
