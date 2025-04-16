82a8cda956bb50229281d4ed264abbda564820ef
Oparin Dmitry <oparin@spk.ru>
2025-04-16 16:03:08 +0500

    fix apply_restrictions and db

--- gpt_db/data/fill_authority_db.py
+++ gpt_db/data/fill_authority_db.py
@@ -7,21 +7,19 @@ cursor = connection.cursor()
 
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS ZARM_AUTH_CFO (
-record INTEGER,
-zuser TEXT,
-zvobj TEXT,
-auth TEXT,
-PRIMARY KEY (record, zuser, zvobj)
+zuser TEXT PRIMARY KEY,
+auth TEXT
 )
 ''')
 
 data_for_zarm_auth_cfo = [
-    (1, 'user1', '7.117', "(ZCFO1 in ('4505','1002','1102','1303','1403','1502','2402',) or (ZDIV = '04'))"),
-    (2, 'user1', '7*', "(ZDIV = '03')"),
-    (2, 'user1', '*', "(ZDIV = '01')"),
-    (2, 'user2', '7*', "(ZDIV = '01')"),
+    ('user1', "(ZCFO1 in ('4505','1002','1102','1303','1403','1502','2402',) or (ZDIV = '04'))"),
+    ('user1', "(ZDIV = '03')"),
+    ('user1', "(ZDIV = '01')"),
+    ('user2', "(ZDIV = '01')"),
+    ('user0', ""),
 ]
-insert_query_for_zarm_auth_cfo = """INSERT INTO ZARM_AUTH_CFO (record, zuser, zvobj, auth) VALUES (?, ?, ?, ?);"""
+insert_query_for_zarm_auth_cfo = """INSERT INTO ZARM_AUTH_CFO (zuser, auth) VALUES (?, ?);"""
 cursor.executemany(insert_query_for_zarm_auth_cfo, data_for_zarm_auth_cfo)
 
 
