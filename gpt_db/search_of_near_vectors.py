adfa2d5148db7f66b6b5351bac7e2c2b249479eb
Oparin Dmitry <oparin@spk.ru>
2025-05-23 09:57:35 +0500

    add search_by_referenses

--- gpt_db/search_of_near_vectors.py
+++ gpt_db/search_of_near_vectors.py
@@ -6,10 +6,13 @@ from pathlib import Path
 model = SentenceTransformer('cointegrated/rubert-tiny2')
 path_to_db = Path(__file__).parent / 'data' / 'sqlite.db'
 
-def search_of_near_vectors(string, *key_strings) -> list[tuple[str, float, str, str]]:
+def search_of_near_vectors(strings) -> dict[str, list[tuple[str, float, str, str]]]:
     """
     Example of return:
-    [('ZPRODH31', 0.06224990263581276, 'Арматура 10 А400', '100100140001900'), ('ZPRODH21', 0.13169412314891815, 'Каркас треуг 8 А500С', '3054004700'), ...]
+    {
+    'Арматура десятка А400': [('ZPRODH31', 0.06224990263581276, 'Арматура 10 А400', '100100140001900'), ('ZPRODH21', 0.13169412314891815, 'Каркас треуг 8 А500С', '3054004700'), ...]
+    'филиал екатеринбург': [('ZCFO1', 0.16964463889598846, 'СПК-Екатеринбург', '0802'), ('ZCUSTOMER', 0.18706147372722626, 'ООО Ветрикаль', '9000719800'), ...]
+    }    
     """
     connection = sqlite3.connect(path_to_db)
     connection.enable_load_extension(True)
@@ -19,38 +22,33 @@ def search_of_near_vectors(string, *key_strings) -> list[tuple[str, float, str, 
     names_of_tables_query = connection.execute("select name from sqlite_master where type='table'")
     names_of_tables = [table[0] for table in names_of_tables_query.fetchall() if table[0] != 'ZARM_AUTH_CFO']
 
-    embedding = model.encode(string)
+    answer = {}
+    for string in strings:
+        answer[string] = []
+        embedding = model.encode(string)
+        for name_of_table in names_of_tables:
+            row = connection.execute(
+                f"""
+                SELECT                    
+                    vec_distance_cosine(VECTOR, ?) as DISTANCE,                    
+                    TXT,
+                    KEY
+                FROM {name_of_table}
+                ORDER BY DISTANCE
+                LIMIT 1
+                """,
+                (sqlite_vec.serialize_float32(embedding),)
+            ).fetchone()
+        
+            to_answer = (name_of_table,) + row
+            answer[string].append(to_answer)
 
-    answer = []
-    for name_of_table in names_of_tables:
-        row = connection.execute(
-            f"""
-            SELECT                    
-                vec_distance_cosine(VECTOR, ?) as DISTANCE,                    
-                TXT,
-                KEY
-            FROM {name_of_table}
-            ORDER BY DISTANCE
-            LIMIT 1
-            """,
-            (sqlite_vec.serialize_float32(embedding),)
-        ).fetchall()
-
-        #проверям содержание key_strings в TXT при наличии key_strings
-        TXT = row[0][1]
-        if key_strings and not all(key_string.upper() in TXT.upper() for key_string in key_strings):
-            continue
-
-        to_answer = (name_of_table,) + row[0]
-        answer.append(to_answer)
+        answer[string].sort(key=lambda x: x[1])
 
     connection.close()
-
-    answer.sort(key=lambda x: x[1])
-    if answer: return answer
+    return answer
 
 
 if __name__ == "__main__":
-    res = search_of_near_vectors('Арматура десятка А400', 'Арматура', 'А400')
-    res = search_of_near_vectors('Арматура десятка А400')
-    print(res)
+    test_query = ['Арматура десятка А400', 'филиал екатеринбург']
+    print(search_of_near_vectors(test_query))
